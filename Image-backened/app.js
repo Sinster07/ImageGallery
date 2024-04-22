fce3f44c9027dd3c49f07ecceccc711c4e0b7b18
Sanchit Srivastava <103961250+Sinster07@users.noreply.github.com>
2024-04-22 10:47:53 +0530

    GET

--- Image-backened/app.js
+++ Image-backened/app.js
@@ -14,12 +14,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Methods",
-    GET,
-    HEAD,
-    OPTIONS,
-    POST,
-    PUT,
-    DELETE
+    "GET,HEAD, OPTIONS, POST, PUT, DELETE"
   );
   res.header(
     "Access-Control-Allow-Methods",
